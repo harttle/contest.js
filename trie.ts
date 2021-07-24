d13ab45e1d9fb693afa1ae0e93d351d66b7ea91c
Yiming Li <yimingli.cs@gmail.com>
2021-07-25 02:35:38 +0800

    feat: trie refine according to review

--- trie.ts
+++ trie.ts
@@ -1,103 +1,78 @@
+class TrieNode {
+  count: number = 0
+  countPrefix: number = 0
+  children: { [key: string]: TrieNode | undefined } = {}
+};
+
 class Trie {
-  m: number
-  trie: number[][]
-  /* Count of a node */
-  cnt: number[]
-  /* Count as prefix of a node */
-  cntPre: number[]
-  /* Number of nodes in the trie */
-  tot: number
-  s: number
+  readonly trie: TrieNode
   /**
-     * @description Initialize the trie
-     * @param m the range of the chars
-     * @param s the smallest char in the string, assuming all chars are in range [s, s+m)
-     */
-  constructor (m: number, s = 'a') {
-    this.m = m
-    this.trie = [[]]
-    this.cnt = []
-    this.cntPre = []
-    // Node 1 is the root node
-    this.tot = 1
-    this.s = s.charCodeAt(0)
+   * @description Initialize the trie
+   */
+  constructor () {
+    this.trie = new TrieNode()
   }
 
   /**
-     * @description Insert strings into the trie
-     * @param x the string to be inserted
-     * @param t number of `x` to be inserted, can be negative for deleting x from the trie
-     */
-  insert (x: string, t: number = 1): void {
-    let p = 1
-    for (let i = 0; i < x.length; i++) {
-      const idx = x.charCodeAt(i) - this.s
-      this.trie[p] ??= Array(this.m).fill(0)
-      if (this.trie[p][idx] === 0) {
-        this.trie[p][idx] = ++this.tot
-      }
-      p = this.trie[p][idx]
-      this.cntPre[p] ??= 0
-      this.cntPre[p] += t
+   * @description Insert strings into the trie
+   * @param str the string to be inserted
+   * @param count number of `str` to be inserted, can be negative for deleting x from the trie
+   */
+  insert (str: string, count: number = 1): void {
+    let cur = this.trie
+    for (const char of str) {
+      cur.children[char] ??= new TrieNode()
+      cur = cur.children[char]!
+      cur.countPrefix += count
     }
-    this.cnt[p] ??= 0
-    this.cnt[p] += t
+    cur.count += count
   }
 
   /**
-     * @description Search a string in the trie
-     * @returns Number of `x` in the trie
-     */
-  search (x: string): number {
-    let p = 1
-    for (let i = 0; i < x.length; i++) {
-      const idx = x.charCodeAt(i) - this.s
-      if (this.trie[p][idx] === 0) {
-        return 0
-      }
-      p = this.trie[p][idx]
+   * Traverse the trie with a callback function
+   * @param str An input string
+   * @param callbackfn A callback function. traverse calls the callbackfn function one time for each char in `str`, however it may skip ending chars in str if the trie has no children to go deeper, the returned char from callbackfn will be used as the direction of traversing
+   * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value
+   */
+  traverse (str: string, callbackfn: (char: string, idx: number, node: TrieNode) => string, thisArg?: any): void {
+    let cur = this.trie
+    for (let i = 0; i < str.length; i++) {
+      const retChar = callbackfn.call(thisArg, str[i], i, cur)
+      const tmp = cur.children[retChar]
+      if (!tmp || tmp.countPrefix < 0) return
+      cur = tmp
     }
-    return this.cnt[p]
   }
 
   /**
-     * @description Search a string in the trie
-     * @returns Number of prefix of `x` in the trie
-     */
-  searchPrefix (x: string): number {
-    let p = 1
+   * @description Search a string in the trie
+   * @returns Number of `str` in the trie
+   */
+  count (str: string): number {
     let ans = 0
-    for (let i = 0; i < x.length; i++) {
-      const idx = x.charCodeAt(i) - this.s
-      if (this.trie[p][idx] === 0) {
-        return ans
+    this.traverse(str, (char, idx, node) => {
+      const nextNode = node.children[char]
+      if (idx === str.length - 1 && nextNode) {
+        ans = nextNode.count
       }
-      p = this.trie[p][idx]
-      ans += this.cntPre[p]
-    }
+      return char
+    })
     return ans
   }
 
   /**
-     * @description Search a string in the trie
-     * @returns Maximum XOR of `x` in the trie
-     */
-  searchMaxXor (x: string): string {
-    let p = 1
-    const ans = []
-    for (let i = 0; i < x.length; i++) {
-      const idx = x.charCodeAt(i) - this.s
-      // Go in opposite direction as possible
-      if (this.cntPre[this.trie[p][this.m - 1 - idx]]) {
-        p = this.trie[p][this.m - 1 - idx]
-        ans.push(this.s + this.m - 1 - idx)
-      } else {
-        p = this.trie[p][idx]
-        ans.push(this.s + idx)
-      }
-    }
-    return String.fromCharCode(...ans)
+   * @description Search a string in the trie
+   * @returns Number of prefix of `str` in the trie
+   */
+  countPrefix (str: string): number {
+    let ans = 0
+    this.traverse(str, (char, idx, node) => {
+      const nextNode = node.children[char]
+      ans += nextNode?.countPrefix ?? 0
+      return char
+    })
+    return ans
   }
 };
 
-export { Trie }
+export { TrieNode, Trie }
