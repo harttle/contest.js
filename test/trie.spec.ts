853dca0fd3d9bbe92869e62c9b7d2bdbfd34cc1a
Yiming Li <yimingli.cs@gmail.com>
2021-07-25 02:35:38 +0800

    feat: trie refine according to review

--- test/trie.spec.ts
+++ test/trie.spec.ts
@@ -3,44 +3,57 @@ import { Trie } from '../trie'
 describe('trie', () => {
   describe('Trie', () => {
     it('add successfully', () => {
-      const trie = new Trie(2, 'a')
+      const trie = new Trie()
       const arr = ['aaa', 'aab', 'a', 'b']
       arr.forEach((s) => trie.insert(s), trie)
-      expect(arr.map(trie.search, trie)).toEqual([1, 1, 1, 1])
+      expect(arr.map(trie.count, trie)).toEqual([1, 1, 1, 1])
     })
     it('remove successfully', () => {
-      const trie = new Trie(2, 'a')
+      const trie = new Trie()
       const arr = ['aaa', 'aab', 'a', 'b', 'a', 'aab']
       arr.forEach((s) => trie.insert(s), trie)
       const removeArr = ['aaa', 'aab']
       removeArr.forEach((s) => trie.insert(s, -1), trie)
-      expect(trie.search('aaa')).toEqual(0)
-      expect(trie.search('aab')).toEqual(1)
-      expect(trie.search('a')).toEqual(2)
-      expect(trie.search('b')).toEqual(1)
+      expect(trie.count('aaa')).toEqual(0)
+      expect(trie.count('aab')).toEqual(1)
+      expect(trie.count('a')).toEqual(2)
+      expect(trie.count('b')).toEqual(1)
     })
     it('searchPrefix successfully', () => {
-      const trie = new Trie(3, 'a')
+      const trie = new Trie()
       const arr = ['aaa', 'aab', 'a', 'b', 'a', 'aab']
       arr.forEach((s) => trie.insert(s), trie)
-      expect(trie.searchPrefix('aaa')).toEqual(9)
-      expect(trie.searchPrefix('aab')).toEqual(10)
-      expect(trie.searchPrefix('a')).toEqual(5)
-      expect(trie.searchPrefix('b')).toEqual(1)
-      expect(trie.searchPrefix('c')).toEqual(0)
+      expect(trie.countPrefix('aaa')).toEqual(9)
+      expect(trie.countPrefix('aab')).toEqual(10)
+      expect(trie.countPrefix('a')).toEqual(5)
+      expect(trie.countPrefix('b')).toEqual(1)
+      expect(trie.countPrefix('c')).toEqual(0)
     })
-    it('searchMaxXor successfully', () => {
+    it('calculate max xor successfully', () => {
       // Test cases from: https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/
       test([3, 10, 5, 25, 2, 8], 28)
       test([0], 0)
       test([8, 10, 2], 10)
       test([14, 70, 53, 83, 49, 91, 36, 80, 92, 51, 66, 70], 127)
       function test (nums: number[], exp: number): void {
-        const s = nums.map(num => num.toString(2).padStart(32, '0'))
-        const trie = new Trie(2, '0')
-        s.forEach(x => trie.insert(x))
+        const strs = nums.map(num => num.toString(2).padStart(32, '0'))
+        const trie = new Trie()
+        strs.forEach(x => trie.insert(x))
+        const reverse = (ch: string): string => ch === '1' ? '0' : '1'
         let ans = 0
-        s.map(trie.searchMaxXor, trie).forEach((x, idx) => { ans = Math.max(ans, parseInt(x, 2) ^ nums[idx]) })
+        strs.forEach((str, idx) => {
+          let tmp = 0
+          trie.traverse(str, (char, idx, node) => {
+            tmp <<= 1
+            if (node.children[reverse(char)]) {
+              tmp += 1
+              return reverse(char)
+            } else {
+              return char
+            }
+          })
+          ans = Math.max(ans, tmp)
+        })
         expect(ans).toEqual(exp)
       }
     })
